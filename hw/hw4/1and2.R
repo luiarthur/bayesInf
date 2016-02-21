db2ec3ff735fbba1442fc52a7e94d90ff2b0eb29
luiarthur <luiarthur@gmail.com>
2016-02-21 10:42:46 -0800

    yes

--- hw/hw4/1and2.R
+++ hw/hw4/1and2.R
@@ -19,16 +19,26 @@ ci_1a
 (1.5*.5*.1^-.5  + 8.5*7.5*.9^6.5 - 2*1.5*8.5*.1^.5*.9^7.5) * .1^2 /
 (.5*-.5*.1^-1.5 + 8.5*7.5*.9^6.5 - 2* .5*8.5*.1^-.5*.9^7.5) # .1961632
 
-laplace.approx <- function(bn,bd,n,mle,s2_n,s2_d,hn,hd,mle) {
-  bn(mle) / bd(mle) * s2_n(mle,n) / s2_d(mle,n) * exp(-n*(hn(mle,n)-hd(mle,n)))
+pp <- seq(0,1,len=100)
+curve(pp * function(p) dbinom(1,10,p)
+
+laplace.approx <- function(bn,bd,n,tn,td,s2_n,s2_d,hn,hd) {
+  bn(tn) / bd(td) * s2_n(tn,n) / s2_d(td,n) * exp(-(hn(tn,n)-hd(td,n)))
 }
 
-laplace.approx(bn=function(p) 1, bd=function(p) 1, n=1, mle = 1/9,
-               s2_n=function(p,n) 1/ ((1.5/p^2 + 8.5/(1-p)^2) / n),
-               s2_d=function(p,n) 1/ ((0.5/p^2 + 8.5/(1-p)^2) / n),
-               hn=function(p,n) ( dbinom(1,10,p,log=T) + dbeta(p,1+.5+1,10-1+.5,log=T) ) / -n,
-               hd=function(p,n) ( dbinom(1,10,p,log=T) + dbeta(p,1+.5+0,10-1+.5,log=T) ) / -n)
+fd <- function(p) dbinom(1,10,p) * dbeta(p,.5,.5)
+fn <- function(p) p * fd(p)
+curve(fn,from=0,to=1,col='blue',ylim=c(0,.5))
+curve(fd,from=0,to=1,add=T,col='red')
+pp <- seq(0,1,len=500)
+td <- pp[which.max(fd(pp))]
+tn <- pp[which.max(fn(pp))]
 
+laplace.approx(bn=function(p) 1,bd=function(p) 1,n=10,td=td,tn=tn,
+               s2_n=function(p,n) 1/ ((1.5/p^2 + 8.5/(1-p)^2)),
+               s2_d=function(p,n) 1/ ((0.5/p^2 + 8.5/(1-p)^2)),
+               hn=function(p,n) -( dbinom(1,n,p,log=T) + dbeta(p,1+.5+1,n-1+.5,log=T) ),
+               hd=function(p,n) -( dbinom(1,n,p,log=T) + dbeta(p,1+.5+0,n-1+.5,log=T) ))
 
 #1c) 
 samps_1c <- rbeta(1000000,1.5,9.5)
