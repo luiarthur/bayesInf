c55bef3360b3d29c6037f6016a06d183e6d15858
luiarthur <luiarthur@gmail.com>
2016-02-22 12:08:40 -0800

    yes

--- hw/hw4/1and2.R
+++ hw/hw4/1and2.R
@@ -7,39 +7,45 @@ source("../../myfunc/func.R")
 
 ######################################################################
 
-#1) 
-params_1 <- matrix(c(10,1,100,10,1000,100),3,2,byrow=T)
-colnames(params_1) <- c("n","x")
-
 #1a) Beta(1.5,9.5)
-ci_1a <- qbeta(c(.025,.975), params_1[1,2]+.5,params_1[1,1]-params_1[1,2]+.5) # (0.01101167, 0.38131477)
+x <- 1; n <- 10
+ci_1a <- qbeta(c(.025,.975), x+.5, n-x+.5) # (0.01101167, 0.38131477)
 ci_1a
 
 #1b) Robert 6.2.9
+a <- x+.5
+b <- n-x+.5
+post.mean <- a / (a+b)
+post.var <- a*b / ( (a+b)^2 * (a+b+1) )
+ci_1b <- qnorm(c(.025,.975), post.mean, sqrt(post.var)) # (-0.05780193, 0.33052920)
+ci_1b
+
+rm(list=c("a","b")) # removes the variables a, b
+
 #http://www.sumsar.net/blog/2013/11/easy-laplace-approximation/
-(1.5*.5*.1^-.5  + 8.5*7.5*.9^6.5 - 2*1.5*8.5*.1^.5*.9^7.5) * .1^2 /
-(.5*-.5*.1^-1.5 + 8.5*7.5*.9^6.5 - 2* .5*8.5*.1^-.5*.9^7.5) # .1961632
-
-pp <- seq(0,1,len=100)
-curve(pp * function(p) dbinom(1,10,p)
-
-laplace.approx <- function(bn,bd,n,tn,td,s2_n,s2_d,hn,hd) {
-  bn(tn) / bd(td) * s2_n(tn,n) / s2_d(td,n) * exp(-(hn(tn,n)-hd(td,n)))
-}
-
-fd <- function(p) dbinom(1,10,p) * dbeta(p,.5,.5)
-fn <- function(p) p * fd(p)
-curve(fn,from=0,to=1,col='blue',ylim=c(0,.5))
-curve(fd,from=0,to=1,add=T,col='red')
-pp <- seq(0,1,len=500)
-td <- pp[which.max(fd(pp))]
-tn <- pp[which.max(fn(pp))]
-
-laplace.approx(bn=function(p) 1,bd=function(p) 1,n=10,td=td,tn=tn,
-               s2_n=function(p,n) 1/ ((1.5/p^2 + 8.5/(1-p)^2)),
-               s2_d=function(p,n) 1/ ((0.5/p^2 + 8.5/(1-p)^2)),
-               hn=function(p,n) -( dbinom(1,n,p,log=T) + dbeta(p,1+.5+1,n-1+.5,log=T) ),
-               hd=function(p,n) -( dbinom(1,n,p,log=T) + dbeta(p,1+.5+0,n-1+.5,log=T) ))
+#(1.5*.5*.1^-.5  + 8.5*7.5*.9^6.5 - 2*1.5*8.5*.1^.5*.9^7.5) * .1^2 /
+#(.5*-.5*.1^-1.5 + 8.5*7.5*.9^6.5 - 2* .5*8.5*.1^-.5*.9^7.5) # .1961632
+#
+#pp <- seq(0,1,len=100)
+#curve(pp * function(p) dbinom(1,10,p)
+#
+#laplace.approx <- function(bn,bd,n,tn,td,s2_n,s2_d,hn,hd) {
+#  bn(tn) / bd(td) * s2_n(tn,n) / s2_d(td,n) * exp(-(hn(tn,n)-hd(td,n)))
+#}
+#
+#fd <- function(p) dbinom(1,10,p) * dbeta(p,.5,.5)
+#fn <- function(p) p * fd(p)
+#curve(fn,from=0,to=1,col='blue',ylim=c(0,.5))
+#curve(fd,from=0,to=1,add=T,col='red')
+#pp <- seq(0,1,len=500)
+#td <- pp[which.max(fd(pp))]
+#tn <- pp[which.max(fn(pp))]
+#
+#laplace.approx(bn=function(p) 1,bd=function(p) 1,n=10,td=td,tn=tn,
+#               s2_n=function(p,n) 1/ ((1.5/p^2 + 8.5/(1-p)^2)),
+#               s2_d=function(p,n) 1/ ((0.5/p^2 + 8.5/(1-p)^2)),
+#               hn=function(p,n) -( dbinom(1,n,p,log=T) + dbeta(p,1+.5+1,n-1+.5,log=T) ),
+#               hd=function(p,n) -( dbinom(1,n,p,log=T) + dbeta(p,1+.5+0,n-1+.5,log=T) ))
 
 #1c) 
 samps_1c <- rbeta(1000000,1.5,9.5)
